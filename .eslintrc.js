fc3af9ed9d4868b400d2f6ea82ff47a3742c4ce0
Jace Babin <jbabin91@gmail.com>
2023-05-31 17:48:07 -0700

    chore: add linting to commits and linting for staged files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ['custom'],
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
   root: true,
   settings: {
     next: {
