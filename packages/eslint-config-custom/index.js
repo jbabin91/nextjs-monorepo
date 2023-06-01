90feb871ede9268bb40bb86794435e8ccc0df8b2
Jace Babin <jbabin91@gmail.com>
2023-05-31 17:00:38 -0700

    chore(lint): update eslint config
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,61 @@
 module.exports = {
-  extends: ['next', 'turbo', 'eslint:recommended', 'prettier'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-  },
+  extends: [
+    'next',
+    'turbo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve('next/babel')],
+    },
+  },
+  plugins: ['simple-import-sort', 'import', 'sort-keys-fix'],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        shorthandFirst: true,
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-keys-fix/sort-keys-fix': 'warn',
+  },
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
   },
 };
